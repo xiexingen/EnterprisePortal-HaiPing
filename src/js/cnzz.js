da833c03f8cad2a5c69034c85f0951d763a519b8
xiexingen <1002275364@qq.com>
2019-08-10 22:49:45 +0800

    增加cnzz

---
+++ src/js/cnzz.js
@@ -0,0 +1,6 @@
+$(function(){
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?dd70728e74da39ee0202093d8080c3db";
+    var s = document.getElementsByTagName("script")[0]; 
+    s.parentNode.insertBefore(hm, s);
+})
