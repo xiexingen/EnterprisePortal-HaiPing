4eefc511e2146897d7598aad7f29cb486ff28f08
xiexingen <1002275364@qq.com>
2019-08-10 17:47:09 +0800

    完成

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,36 +1,35 @@
-window.onload=function(){
-    // 初始化地图
-      var map = new BMap.Map("contact_map");
-      var point = new BMap.Point(116.466723,39.921832);
-      map.enableScrollWheelZoom();
-      map.centerAndZoom(point, 20);
-      map.addControl(new BMap.NavigationControl());
-      map.addControl(new BMap.ScaleControl());
-      map.addControl(new BMap.OverviewMapControl());
-      map.addControl(new BMap.MapTypeControl());
+$(function() {
+  // 初始化地图
+  var map = new BMap.Map("contact_map");
+  var point = new BMap.Point(112.991778,28.158485);
+  map.enableScrollWheelZoom();
+  map.centerAndZoom(point, 20);
+  map.addControl(new BMap.NavigationControl());
+  map.addControl(new BMap.ScaleControl());
+  map.addControl(new BMap.OverviewMapControl());
+  map.addControl(new BMap.MapTypeControl());
 
-      // 创建小蓝点
-      var marker = new BMap.Marker(point);  // 创建标注
-      map.addOverlay(marker);               // 将标注添加到地图中
-      marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
+  // 创建小蓝点
+  var marker = new BMap.Marker(point); // 创建标注
+  map.addOverlay(marker); // 将标注添加到地图中
+  marker.setAnimation(BMAP_ANIMATION_BOUNCE); //跳动的动画
 
-      var opts2 = {
-        position : point,    // 指定文本标注所在的地理位置
-        offset   : new BMap.Size(-40, -60)    //设置文本偏移量
-      }
-      var label2 = new BMap.Label("阳光企服", opts2);  // 创建文本标注对象
-          label2.setStyle({
-               color : "#333",
-               fontSize : "0.24rem",
-              paddingLeft:"0.15rem",
-              paddingRight:"0.15rem",
-               lineHeight : "0.44rem",
-               fontFamily:"微软雅黑",
-               textAlign:"center",
-               background: "#fff",
-               border:"1px solid #e5e5e5",
-               borderRadius:"0.1rem" 
-           });
-      map.addOverlay(label2); 
-}
-
+  var opts2 = {
+    position: point, // 指定文本标注所在的地理位置
+    offset: new BMap.Size(-40, -60) //设置文本偏移量
+  };
+  var label2 = new BMap.Label("长沙迪博财务服务有限公司", opts2); // 创建文本标注对象
+  label2.setStyle({
+    color: "#333",
+    fontSize: "0.24rem",
+    paddingLeft: "0.15rem",
+    paddingRight: "0.15rem",
+    lineHeight: "0.44rem",
+    fontFamily: "微软雅黑",
+    textAlign: "center",
+    background: "#fff",
+    border: "1px solid #e5e5e5",
+    borderRadius: "0.1rem"
+  });
+  map.addOverlay(label2);
+});
--- src/js/lunbo.js
+++ src/js/lunbo.js
@@ -1,38 +1,45 @@
-$(function(){
-//banner
-var swiper = new Swiper('.swiper-containero', {
-    pagination: '.swiper-pagination',
+$(function() {
+  //banner
+  var swiper = new Swiper(".swiper-containero", {
+    pagination: ".swiper-pagination",
     paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    loop : true,
-    speed:1500,
-    autoplay:2000
-});
-//阳光头条
-jQuery(".picScroll-top").slide({titCell:".hd ul",mainCell:".bd ul",autoPage:true,effect:"top",autoPlay:true,vis:2,effect:"topLoop"});
+    nextButton: ".swiper-button-next",
+    prevButton: ".swiper-button-prev",
+    loop: true,
+    speed: 1500,
+    autoplay: 2000
+  });
+  //头条
+  jQuery(".picScroll-top").slide({
+    titCell: ".hd ul",
+    mainCell: ".bd ul",
+    autoPage: true,
+    effect: "top",
+    autoPlay: true,
+    vis: 2,
+    effect: "topLoop"
+  });
 
-var swiper = new Swiper('.swiper-containert', {
+  var swiper = new Swiper(".swiper-containert", {
     slidesPerView: 3.1,
     paginationClickable: true,
-    spaceBetween:20,
+    spaceBetween: 20,
     freeMode: true
-});
+  });
 
-var swiper = new Swiper('.swiper-containers', {
-    slidesPerView:2.1,
+  var swiper = new Swiper(".swiper-containers", {
+    slidesPerView: 2.1,
     paginationClickable: true,
-    spaceBetween:7,
+    spaceBetween: 7,
     freeMode: true
+  });
+  var swiper = new Swiper(".Headline .swiper-container", {
+    direction: "vertical",
+    autoplay: 2000,
+    slidesPerView: 2,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    }
+  });
 });
-var swiper = new Swiper('.Headline .swiper-container', {
-	direction: 'vertical',
-	autoplay:2000,
-	slidesPerView: 2,
-	pagination: {
-	el: '.swiper-pagination',
-		clickable: true,
-	},
-});
-
-})
