52c204c70245925b4b1f9188b3a23ef615862c2b
xiexingen <1002275364@qq.com>
2020-08-27 17:11:30 +0800

    发布

--- dibo-mini-program/src/hooks/index.ts
+++
@@ -1,6 +0,0 @@
-import useRequest from './useRequest';
-import useRequestWIthMore from './useRequestWIthMore';
-import useReachBottomEvent from './useReachBottomEvent';
-import usePullDownRefreshEvent from './usePullDownRefreshEvent';
-
-export { useRequest, useRequestWIthMore, useReachBottomEvent, usePullDownRefreshEvent };
--- dibo-mini-program/src/services/user.ts
+++
@@ -1,9 +0,0 @@
-import { post } from '@/utils/request';
-import { APIPREFIX } from '@/constants';
-
-export async function login(data) {
-  return post({
-    url: `${APIPREFIX}token/TokenAuth/WechatLogin`,
-    data,
-  });
-}
